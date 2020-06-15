6546f4de1596627e81379326d565dfe69edb8237
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2020-06-15 10:45:36 +0400

    asnc

--- ThreadTutorial/ThreadTutorial/Program.cs
+++ ThreadTutorial/ThreadTutorial/Program.cs
@@ -9,22 +9,60 @@ namespace ThreadTutorial
 {
     class Program
     {
-        static void Main(string[] args)
+        static  void Main(string[] args)
         {
-            ThreadPool.QueueUserWorkItem((e) =>
-            {
-                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-            });
-            Thread.CurrentThread.IsBackground = true;
-            
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);                       
+            //Task T = new Task(Yaz);
+            //T.ContinueWith((a)=> {
+            //    Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
+
+            //} ,TaskContinuationOptions.OnlyOnRanToCompletion);
+
+
+            //Task T = Task.Factory.StartNew(Yaz);
+            //T.ContinueWith((a) =>
+            //{
+            //    Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
+
+            //}, TaskScheduler.Current);
+
+
+            //Task T = Task.Run(()=> {
+            //    Console.WriteLine("yaz" + Thread.CurrentThread.ManagedThreadId);
+            //});
+
+
+
+            //T.ContinueWith((a) =>
+            //{
+            //    Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
+
+            //}, TaskScheduler.Current);
+
+            askoron();
+            Console.WriteLine("main"+Thread.CurrentThread.ManagedThreadId);           
+            Console.Read();
+        }
+        
+
+        public async  static Task<string> Yaz()
+        {          
+            Console.WriteLine("yaz" + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(4000);
+            return "aa";
         }
 
-        public static void Yaz()
-        {    
-            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-            Console.ReadLine();
+        public async static void  askoron()
+        {
+            var t = await Task.Run(Yaz);
+            Console.WriteLine("gozle" + Thread.CurrentThread.ManagedThreadId);
         }
-}
+
+        public static void son(Task a)
+        {
+            Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
+          
+            
+        }
+    }
    
 }
