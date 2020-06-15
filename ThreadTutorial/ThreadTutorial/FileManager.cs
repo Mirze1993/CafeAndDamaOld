59fb88ea7e77e606e31595bd646af4889413edb6
abbaseliyev1993 <abbaseliyev1993@gmail.com>
2020-06-15 11:49:13 +0400

    create testcase

---
+++ ThreadTutorial/ThreadTutorial/FileManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTutorial
+{
+    public class FileManager
+    {
+        public readonly static object o = new object();
+        public string ReadText()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(@"D:\My C# Projects\New folder\ThreadTutorial\ThreadTutorial\test.txt");
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                string a = "";
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    a += line;
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();                
+                return a;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return "Exception: " + e.Message;
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+
+        public void WriteText(string a)
+        {
+            try
+            {
+                lock (o)
+                {
+                    string b = ReadText() + "/" + a;
+                    //Pass the filepath and filename to the StreamWriter Constructor
+                    StreamWriter sw = new StreamWriter(@"D:\My C# Projects\New folder\ThreadTutorial\ThreadTutorial\test.txt");
+
+                   
+                    //Write a line of text
+                    sw.WriteLine(b);
+                    //Close the file
+                    sw.Close();
+                    Thread.Sleep(3000);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
--- ThreadTutorial/ThreadTutorial/Program.cs
+++ ThreadTutorial/ThreadTutorial/Program.cs
@@ -11,58 +11,9 @@ namespace ThreadTutorial
     {
         static  void Main(string[] args)
         {
-            //Task T = new Task(Yaz);
-            //T.ContinueWith((a)=> {
-            //    Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
-
-            //} ,TaskContinuationOptions.OnlyOnRanToCompletion);
-
-
-            //Task T = Task.Factory.StartNew(Yaz);
-            //T.ContinueWith((a) =>
-            //{
-            //    Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
-
-            //}, TaskScheduler.Current);
-
-
-            //Task T = Task.Run(()=> {
-            //    Console.WriteLine("yaz" + Thread.CurrentThread.ManagedThreadId);
-            //});
-
-
-
-            //T.ContinueWith((a) =>
-            //{
-            //    Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
-
-            //}, TaskScheduler.Current);
-
-            askoron();
-            Console.WriteLine("main"+Thread.CurrentThread.ManagedThreadId);           
-            Console.Read();
-        }
-        
-
-        public async  static Task<string> Yaz()
-        {          
-            Console.WriteLine("yaz" + Thread.CurrentThread.ManagedThreadId);
-            await Task.Delay(4000);
-            return "aa";
-        }
-
-        public async static void  askoron()
-        {
-            var t = await Task.Run(Yaz);
-            Console.WriteLine("gozle" + Thread.CurrentThread.ManagedThreadId);
-        }
-
-        public static void son(Task a)
-        {
-            Console.WriteLine("son" + Thread.CurrentThread.ManagedThreadId);
-          
             
         }
+        
     }
    
 }
